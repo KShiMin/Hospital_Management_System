4792c6cedac7c34d84c696079b5bd1eb45f9d81e
ZendenKwok <73163975+ZendenKwok@users.noreply.github.com>
2025-02-04 12:36:46 +0800

    Update Main.java
    Added to print billing details with discount and tax

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+        // Creating an invoice with discount and tax details
         Invoice invoice = new Invoice(
                 "INV12345",
                 new Date(),
@@ -9,8 +10,10 @@ public class Main {
                 "Payment due in 31 days."
         );
 
-        // Display details
-        System.out.println(invoice);
+        // Setting billing details
+        invoice.setBillingDetails(1000, 10, 5);
+
+        // Display invoice details including discount and tax
         invoice.printInvoice();
     }
 }
