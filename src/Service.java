a9e3dab163e03a438f4e0ebccd0a084d8bb5cfcd
Natalie <212856U@mymail.nyp.edu.sg>
2025-02-06 01:03:33 +0800

    added quantity and unit price into Service class

--- src/Service.java
+++ src/Service.java
@@ -6,16 +6,20 @@ public class Service {
     // Variables
     private String serviceCode, serviceDescript;
     private Date serviceDate;
+    private int quantity;
+    private double unitPrice;
 
     // Constructors
     public Service(){
-        this("None", "None", new Date());
+        this("None", "None", new Date(), 0, 0.00);
     }
 
-    public Service(String sc, String sd, Date sdate){
+    public Service(String sc, String sd, Date sdate, int q, double pr){
         this.serviceCode = sc;
         this.serviceDescript = sd;
         this.serviceDate = sdate;
+        this.quantity = q;
+        this.unitPrice = pr;
     }
 
     // Setters
@@ -31,6 +35,14 @@ public class Service {
         this.serviceDate = sdate;
     }
 
+    public void setQuantity(int q){
+        this.quantity = q;
+    }
+
+    public void setUnitPrice(double pr){
+        this.unitPrice = pr;
+    }
+
     // Getters
     public String getServiceCode(){
         return this.serviceCode;
@@ -40,7 +52,30 @@ public class Service {
         return this.serviceDescript;
     }
 
-    public Date setServiceDate(){
+    public Date getServiceDate(){
         return this.serviceDate;
     }
+
+    public int getQuantity(){
+        return this.quantity;
+    }
+
+    public double getUnitPrice(){
+        return this.unitPrice;
+    }
+
+    public double calculatePrice() {
+        return this.quantity * this.unitPrice;
+    }
+
+    public void displayService() {
+        System.out.println("Service Code: " + this.serviceCode);
+        System.out.println("Service Description: " + this.serviceDescript);
+        System.out.println("Service Date: " + this.serviceDate);
+        System.out.println("Service Quantity: " + this.quantity);
+        System.out.println("Service Unit Price: " + this.unitPrice);
+        System.out.println("Service Price: " + calculatePrice());
+    }
+
 }
+
