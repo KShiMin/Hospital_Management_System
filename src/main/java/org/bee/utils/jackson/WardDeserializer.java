f3e8fa3bd44985aa539ffc42fafa6d9491739a3a
2400912 <2400912@sit.singaporetech.edu.sg>
2025-03-30 15:01:20 +0800

    javadocs

--- src/main/java/org/bee/utils/jackson/WardDeserializer.java
+++ src/main/java/org/bee/utils/jackson/WardDeserializer.java
@@ -10,7 +10,41 @@ import org.bee.hms.wards.WardFactory;
 
 import java.io.IOException;
 
+/**
+ * Custom Jackson deserializer for {@link Ward} objects.
+ * <p>
+ * This deserializer converts a JSON object representing a ward into a {@link Ward} instance.
+ * The expected JSON structure must contain:
+ * <ul>
+ *     <li><b>name</b> - the name of the ward (string)</li>
+ *     <li><b>classType</b> - the class type of the ward (string, matching {@link WardClassType})</li>
+ * </ul>
+ * </p>
+ *
+ * <pre>
+ * Example JSON:
+ * {
+ *     "name": "General Ward 1",
+ *     "classType": "GENERAL"
+ * }
+ * </pre>
+ *
+ * This will be deserialized into a {@code Ward} instance using {@link WardFactory#getWard(String, WardClassType)}.
+ */
 public class WardDeserializer extends JsonDeserializer<Ward> {
+
+    /**
+     * Deserializes a {@link Ward} object from a JSON representation.
+     * <p>
+     * Reads the {@code name} and {@code classType} fields from the JSON and creates a {@link Ward}
+     * using the {@link WardFactory}.
+     * </p>
+     *
+     * @param p     The {@link JsonParser} for reading the JSON input.
+     * @param ctxt  The {@link DeserializationContext} that can provide additional context for deserialization.
+     * @return A {@link Ward} object representing the data in the JSON.
+     * @throws IOException if the JSON cannot be parsed correctly or required fields are missing.
+     */
     @Override
     public Ward deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
         JsonNode node = p.getCodec().readTree(p);
